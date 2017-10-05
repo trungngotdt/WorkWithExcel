36c34022102f174ab12a0ed391c09cb152b3d122
trungngotdt <trungngotdt@outlook.com.vn>
2017-10-05 16:31:25 +0700

    read file excel

---
+++ WorkWithExcel/ExcelReader.cs
@@ -0,0 +1,50 @@
+﻿using ExcelDataReader;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace WorkWithExecl
+{
+    public class ExcelReader
+    {
+        private String address;
+
+        public String Address
+        {
+            get { return address; }
+            set { address = value; }
+        }
+        public ExcelReader()
+        {
+
+        }
+        public ExcelReader(string ar)
+        {
+            this.Address = ar;
+        }
+        public List<object> GetData()
+        {
+            var fs = File.Open(Address,FileMode.Open,FileAccess.Read);
+            IExcelDataReader reader = ExcelReaderFactory.CreateReader(fs);
+            List<object> list = new List<object>();
+            object value;
+            while (reader.Read())
+            {
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    value = reader.GetValue(i);
+                    list.Add(value);
+                    list.Add("\t");
+                }
+                list.Add("\n");
+            }
+            return list;
+        }
+
+    }
+}
---
+++ WorkWithExcel/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkWithExecl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var address =Directory.GetParent( Directory.GetCurrentDirectory()).Parent.FullName.ToString()+ "\\Example.xlsx";
+            ExcelReader excelReader = new ExcelReader(address);
+            List<object> list= excelReader.GetData();
+            
+            list.ForEach((x) =>
+            {
+                Console.Write(x.ToString());
+            });
+            Console.ReadLine();
+        }
+    }
+}
